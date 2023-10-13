4a3a48470d357534f8cf3b11ebdb75788e3c4790
Andrew Alsing <andrew32alsing@gmail.com>
2023-10-13 12:10:51 -0700

    added pistol pickup animation

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -21,6 +21,12 @@ public class Pistol : MonoBehaviour
     public float returnSpeed; // default: 40f
     public float slideZDistance; // default: -0.2f
 
+    [Header("Gun Slinger Animation")]
+    public float sideTurnDuration = 0.25f;
+    public float horizontalSpinDuration = 0.5f;
+    public float verticalSpinDuration = 0.5f;
+    private bool isAnimatingGunSlinger = false;
+
     [Header("References")]
     public Transform shootPoint;
     public Transform slideTransform;
@@ -34,18 +40,21 @@ public class Pistol : MonoBehaviour
         // store original pistol rotation and slide position
         originalRotation = transform.localEulerAngles;
         originalSlidePosition = slideTransform.localPosition;
+
+        StartCoroutine(GunSlingerTrick());
     }
 
     void Update()
     {
-        if (isReloading)
+        if (isReloading || isAnimatingGunSlinger)
             return;
 
         if (Input.GetButtonDown("Fire1") && currentAmmo > 0)
         {
-            Shoot();
-            Kickback();
-            MoveSlide();
+            StartCoroutine(GunSlingerTrick());
+            // Shoot();
+            // Kickback();
+            // MoveSlide();
         }
 
         if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo && reserveAmmo > 0)
@@ -131,6 +140,49 @@ public class Pistol : MonoBehaviour
         }
 
         isReloading = false;
+    }
+
+    IEnumerator GunSlingerTrick()
+    {
+        isAnimatingGunSlinger = true;
+
+        // 1. rotate the pistol's z-axis to 90 degrees.
+        Quaternion sideRotation = Quaternion.Euler(originalRotation.x, originalRotation.y, originalRotation.z + 90);
+        float startTime = Time.time;
+        while (Time.time < startTime + sideTurnDuration)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, sideRotation, (Time.time - startTime) / sideTurnDuration);
+            yield return null;
+        }
+
+        // 2. spin the pistol horizontally
+        startTime = Time.time;
+        while (Time.time < startTime + horizontalSpinDuration)
+        {
+            transform.Rotate(360f / horizontalSpinDuration * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+
+        // 3. rotate the pistol's z-axis back to 0.
+        startTime = Time.time;
+        while (Time.time < startTime + sideTurnDuration)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(originalRotation), (Time.time - startTime) / sideTurnDuration);
+            yield return null;
+        }
+
+        // 4. spin the pistol vertically.
+        startTime = Time.time;
+        while (Time.time < startTime + verticalSpinDuration)
+        {
+            transform.Rotate(360f / verticalSpinDuration * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+
+        // reset the pistol's rotation to the original position.
+        transform.localRotation = Quaternion.Euler(originalRotation);
+
+        isAnimatingGunSlinger = false;
     }
 }
 
